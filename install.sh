e70a72314f8977f2f8347676bfb0d92062d1b46e
icw-rkhan <47011862+icw-rkhan@users.noreply.github.com>
2019-08-29 17:28:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-export ENDE_HOME="$HOME/rkhan/pipeline-ende"
-python3 "$ENDE_HOME/ende.py" --install
-chmod 777 "$ENDE_HOME/ende.sh"
-chmod 777 "$ENDE_HOME/test.sh" 
-export PATH=$PATH:$ENDE_HOME
+ENDE_HOME="/devtools/pipeline-ende"
+sudo python "$ENDE_HOME/ende.py" --install
 echo [pipeline-ende] install done.
+
+echo "---Running Test -Begin---"
+test="raja_khan"
+x="$(python "$ENDE_HOME/ende.py" --encr $test)"
+y="$(python "$ENDE_HOME/ende.py" --decr $x)"
+
+
+if [ "$test" = "$y" ]; then
+    echo "Test Passed"
+else
+    echo "Test Failed: $y"
+fi
+
+echo "---Running Test -END---"
